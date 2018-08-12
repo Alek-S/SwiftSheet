298aea7c7bdd1982be38f88a12088ac728fcfdd5
Alek Shnayder <alek.shnayder@gmail.com>
2018-08-11 20:55:13 -0500

    resolve unused var, test prettier

--- controller/routes.js
+++ controller/routes.js
@@ -2,35 +2,35 @@ const SwiftSheet = require('../model/swiftsheetDB');
 const chalk = require('chalk');
 
 module.exports = function(app) {
-	app.get('/api/sanitycheck', (req, res) =>{
-		res.status(200).json({hello: 'world'});
+	app.get('/api/sanitycheck', (_req, res) => {
+		res.status(200).json({ hello: 'world' });
 	});
 
-	app.post('/api/sanitycheck', (req, res) =>{
+	app.post('/api/sanitycheck', (req, res) => {
 		const { message } = req.body;
 
-		if(message){
+		if (message) {
 			SwiftSheet.create({
 				message,
-				active: true
+				active: true,
 			})
-				.then( ()=>{
+				.then(() => {
 					res.status(200).json({
 						success: true,
 						message,
 					});
 				})
-				.catch(err =>{
-					console.log( chalk.red(err));
+				.catch(err => {
+					console.log(chalk.red(err));
 					res.status(500).json({
 						success: false,
-						error: err
+						error: err,
 					});
 				});
-		}else{
+		} else {
 			res.status(400).json({
 				success: false,
-				error: 'No message'
+				error: 'No message',
 			});
 		}
 	});
