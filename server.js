a16f7ea8e0a9d8746080b76420c5687d07c667ff
Alek Shnayder <alek.shnayder@gmail.com>
2018-08-12 21:13:14 -0500

    removed bcrypt, change morgan dev mode logger

--- server.js
+++ server.js
@@ -4,63 +4,68 @@ const mongoose = require('mongoose');
 const chalk = require('chalk');
 const morgan = require('morgan');
 
-
 //==Express Setup==
 const app = express();
-app.set('port', (process.env.PORT || 5000));
-
+app.set('port', process.env.PORT || 5000);
 
 //==Mode==
-if(process.env.NODE_ENV === 'production'){
-	console.log(`Server mode [${chalk.red( process.env.NODE_ENV )}]` );
-}else{
-	console.log(`Server mode [${chalk.cyan('dev')}]` );
+if (process.env.NODE_ENV === 'production') {
+	console.log(`Server mode [${chalk.red(process.env.NODE_ENV)}]`);
+} else {
+	console.log(`Server mode [${chalk.cyan('dev')}]`);
 }
-
 
 //===Morgan Logger Middleware===
 if (process.env.NODE_ENV === 'production') {
-	app.use(morgan(':date[web] :remote-addr -- :method :url :status - :response-time ms'));
+	app.use(
+		morgan(
+			':date[web] :remote-addr -- :method :url :status - :response-time ms'
+		)
+	);
 } else {
-	app.use(morgan(':method :url :status - :response-time ms'));
+	app.use(
+		morgan(
+			`${chalk.blue(':method')} :url  ${chalk.grey(
+				':status | :response-time ms'
+			)}`
+		)
+	);
 }
-
 
 //===Parsing===
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 
-
 //===Static Files, CSS,Images,Fonts===
 app.use(express.static('dist'));
-
 
 //===Trust First Proxy===
 app.set('trust proxy', 1);
 
-
 //===MongoDB Connection with Mongoose==
 mongoose.Promise = global.Promise; //use standard Promise instead of Mongo's promise library
-mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost:27017/swiftsheet', { useNewUrlParser: true });
+mongoose.connect(
+	process.env.MONGODB_URI || 'mongodb://localhost:27017/swiftsheet',
+	{ useNewUrlParser: true }
+);
 const db = mongoose.connection;
 
-db.on('error', error => { // Show any mongoose errors
+db.on('error', error => {
+	// Show any mongoose errors
 	console.error(`${chalk.red('Mongoose Error: ')} ${error}`);
 });
-
 
 //===Routes===
 require('./controller/routes.js')(app);
 
-
 //==Start Server==
 const server = app.listen(app.get('port'), () => {
-	if(process.env.PORT){
-		console.log(`Running on Port: ${app.get('port')}\n` );
-	}else{
+	if (process.env.PORT) {
+		console.log(`Running on Port: ${app.get('port')}\n`);
+	} else {
 		const location = `http://localhost:${app.get('port')}`;
-		console.log(`Running at: ${chalk.cyan(location)}`);
+		console.log(`🙌 Running at: ${chalk.cyan(location)}`);
 	}
 });
 
