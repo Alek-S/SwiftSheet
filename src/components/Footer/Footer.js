53e5b30673f82bbe1f0ae28862c2da81d86dcc84
Alek Shnayder <alek.shnayder@gmail.com>
2019-01-06 22:19:29 -0600

    Added placholder pages, moved default style to root src

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import defaultStyle from '../defaultStyle';
+import defaultStyle from '../../defaultStyle';
 
 /** @function
  * @name Footer
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import defaultStyle from '../defaultStyle';
+import defaultStyle from '../../defaultStyle';
 
 /** @class
  * @name Header
