6e3a14e7131ee9ae4675e764e6f31134ee68176e
Alek Shnayder <alek.shnayder@gmail.com>
2019-01-12 12:45:57 -0600

    minor: remove hmr dist files

--- dist/vendor.d95f768f0e28726720e9.hot-update.js
+++
@@ -1,9 +0,0 @@
-webpackHotUpdate('vendor', {
-	/***/ './node_modules/attr-accept/dist/index.js': false,
-
-	/***/ './node_modules/react-dropzone/dist/es/index.js': false,
-
-	/***/ './node_modules/react-dropzone/dist/es/utils/index.js': false,
-
-	/***/ './node_modules/react-dropzone/dist/es/utils/styles.js': false,
-});
