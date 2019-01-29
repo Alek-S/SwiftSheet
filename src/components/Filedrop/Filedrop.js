2731ca514d62697ac3a1fcfc9a00b7ba598ef5e4
Alek Shnayder <alek.shnayder@gmail.com>
2019-01-28 22:20:36 -0600

    dropzone styling, color change for dragging

--- src/components/Filedrop/Filedrop.js
+++ src/components/Filedrop/Filedrop.js
@@ -1,35 +1,73 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
-import Dropzone from 'react-dropzone'
+import Dropzone from 'react-dropzone';
+import defaultStyle from '../../defaultStyle';
 
-
-/** @class 
+/** @class
  * @name Filedrop
  * Area for drag & drop file uploads of csv
- * 
+ *
  * @returns {JSX}
-*/
+ */
 export default class Filedrop extends Component {
 	constructor(props) {
 		super(props);
-		this.state = { 
+		this.state = {
 			file: null,
-			rejectedFile: [], 
+			rejectedFile: [],
+			dragging: false,
 		};
-	  }
+	}
 
 	onDrop = (file, rejectedFile) => {
 		// do stuff with files...
+		console.log(file);
+		console.log(file[0].preview);
 		this.setState({ file, rejectedFile });
-	}
+	};
 
-	render(){
-		return(
-			<section>
-				<Dropzone onDrop={this.onDrop} accept="text/csv">
-					<p>Drag & Drop your file here</p>
-				</Dropzone>
+	onDragEnter = () => this.setState({ dragging: true });
+
+	onDragLeave = () => this.setState({ dragging: false });
+
+	render() {
+		const { dragging } = this.state;
+
+		return (
+			<section style={{ height: 'calc(100vh - 200px)' }}>
+				<StyledDropzone
+					onDrop={this.onDrop}
+					accept="text/csv"
+					maxSize={5000000}
+					minSize={1}
+					multiple={false}
+					dragging={dragging}
+					onDragEnter={this.onDragEnter}
+					onDragLeave={this.onDragLeave}
+				>
+					<StyledText>{dragging ? 'Drop' : 'Drag'} CSV file here</StyledText>
+				</StyledDropzone>
 			</section>
-		)
+		);
 	}
 }
+
+const StyledDropzone = styled(Dropzone)`
+	background-color: ${props =>
+		props.dragging ? props.theme.color.blue : props.theme.color.background};
+	border: dashed 1px
+		${props =>
+			props.dragging ? props.theme.color.blue : props.theme.color.border};
+	border-radius: 4px;
+	height: 250px;
+	width: 60%;
+	margin: auto;
+	margin-top: 4rem;
+`;
+
+const StyledText = styled(defaultStyle)`
+	font-size: 1.2rem;
+	text-align: center;
+	background-color: transparent;
+	padding-top: 7rem;
+`;
--- src/pages/UploadPage/UploadPage.js
+++ src/pages/UploadPage/UploadPage.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
 import defaultStyle from '../../defaultStyle';
+import Filedrop from '../../components/Filedrop/Filedrop';
 
 const StyledDiv = styled(defaultStyle)`
-	padding-top: 20rem;
+	padding-top: 7rem;
+	padding-bottom: 2rem;
+	padding-left: 2rem;
+	font-size: 1.2rem;
 `;
 
-const UploadPage = () => <StyledDiv>Upload Page</StyledDiv>;
+const UploadPage = () => (
+	<div>
+		<StyledDiv>Upload Page</StyledDiv>
+		<Filedrop />
+	</div>
+);
 
 export default UploadPage;
