3a305332542966b13da79f2f74ceb561a1085b30
Alek Shnayder <alek.shnayder@gmail.com>
2019-02-05 22:14:21 -0600

    Added Apollo-client apollo-boost, Wrapped <Apollo Provider />

--- src/Root.js
+++ src/Root.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 import theme from './theme';
-
 import App from './pages/App.js';
 
+const client = new ApolloClient({
+	uri: 'http://localhost:5000/graphql', //TODO
+});
+
 const Root = () => (
-	<ThemeProvider theme={theme}>
-		<App />
-	</ThemeProvider>
+	<ApolloProvider client={client}>
+		<ThemeProvider theme={theme}>
+			<App />
+		</ThemeProvider>
+	</ApolloProvider>
 );
 
 ReactDOM.render(<Root />, document.getElementById('app'));
