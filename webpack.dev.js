551317e0659e0d33862108970371d0566302e340
Alek Shnayder <alek.shnayder@gmail.com>
2019-05-17 18:14:07 -0500

    Added hash to scripts for cache busting, generate html template

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
@@ -9,7 +10,7 @@ module.exports = {
 
 	// compiled output js
 	output: {
-		filename: 'assets/js/[name].js',
+		filename: 'assets/js/[name].[hash].js',
 	},
 
 	module: {
@@ -43,9 +44,57 @@ module.exports = {
 	},
 
 	plugins: [
+		new HtmlWebpackPlugin({
+			inject: false,
+			template: require('html-webpack-template'),
+			title: 'SwiftSheet',
+			lang: 'en-US',
+			appMountId: 'app',
+			meta: [
+				{
+					name: 'viewport',
+					content: 'width=device-width, initial-scale=1.0',
+				},
+				,
+				{
+					name: 'author',
+					content: 'Alek Shnayder',
+				},
+				{
+					name: 'description',
+					content:
+						'Upload a CSV file to quickly share a link to a interactive spreadsheet.',
+				},
+			],
+			links: [
+				'https://fonts.googleapis.com/css?family=Poiret+One&text=SwiftSheet|Quicksand',
+				'https://fonts.googleapis.com/icon?family=Material+Icons',
+				{
+					href: '/assets/css/reset.css',
+					rel: 'preload',
+					as: 'style',
+				},
+				{
+					href: './assets/css/reset.css',
+					rel: 'stylesheet',
+				},
+				{
+					href: './assets/images/favicon/favicon-32x32.png',
+					rel: 'icon',
+					sizes: '32x32',
+					type: 'image/png',
+				},
+				{
+					href: './assets/images/favicon/favicon-16x16.png',
+					rel: 'icon',
+					sizes: '16x16',
+					type: 'image/png',
+				},
+			],
+		}),
 		new webpack.HotModuleReplacementPlugin(),
 		new MiniCssExtractPlugin({
-			filename: 'assets/css/[name].css',
+			filename: 'assets/css/[name].[hash].css',
 		}),
 	],
 
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,4 +1,5 @@
-const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const PreloadWebpackPlugin = require('preload-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const UglifyWebpackPlugin = require('uglifyjs-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
@@ -15,7 +16,7 @@ module.exports = {
 
 	// compiled output js
 	output: {
-		filename: 'assets/js/[name].js',
+		filename: 'assets/js/[name].[contenthash].js',
 	},
 
 	module: {
@@ -44,9 +45,66 @@ module.exports = {
 	}, //end module
 
 	plugins: [
+		new HtmlWebpackPlugin({
+			inject: false,
+			template: require('html-webpack-template'),
+			title: 'SwiftSheet',
+			lang: 'en-US',
+			appMountId: 'app',
+			meta: [
+				{
+					name: 'viewport',
+					content: 'width=device-width, initial-scale=1.0',
+				},
+				,
+				{
+					name: 'author',
+					content: 'Alek Shnayder',
+				},
+				{
+					name: 'description',
+					content:
+						'Upload a CSV file to quickly share a link to a interactive spreadsheet.',
+				},
+			],
+			links: [
+				'https://fonts.googleapis.com/css?family=Poiret+One&text=SwiftSheet|Quicksand',
+				'https://fonts.googleapis.com/icon?family=Material+Icons',
+				{
+					href: '/assets/css/reset.css',
+					rel: 'preload',
+					as: 'style',
+				},
+				{
+					href: './assets/css/reset.css',
+					rel: 'stylesheet',
+				},
+				{
+					href: './assets/images/favicon/favicon-32x32.png',
+					rel: 'icon',
+					sizes: '32x32',
+					type: 'image/png',
+				},
+				{
+					href: './assets/images/favicon/favicon-16x16.png',
+					rel: 'icon',
+					sizes: '16x16',
+					type: 'image/png',
+				},
+			],
+		}),
+		new PreloadWebpackPlugin({
+			rel: 'preload',
+			as(entry) {
+				if (/\.css$/.test(entry)) return 'style';
+				if (/\.woff$/.test(entry)) return 'font';
+				if (/\.png$/.test(entry)) return 'image';
+				return 'script';
+			},
+		}),
 		// new BundleAnalyzerPlugin(),
 		new MiniCssExtractPlugin({
-			filename: 'assets/css/[name].css',
+			filename: 'assets/css/[name].[contenthash].css',
 		}),
 	],
 
