a13247396e73633759e85ae4cb38bf1eace1955e
Alek Shnayder <alek.shnayder@gmail.com>
2019-06-16 21:41:54 -0500

    Added flex and media tag for mobile responsive footer, update twitter handle

--- frontend/components/Footer/Footer.js
+++ frontend/components/Footer/Footer.js
@@ -12,30 +12,38 @@ import { Link } from 'react-router-dom';
 export default function Footer() {
 	return (
 		<Styledfooter>
-			Created with 🍷 by{' '}
-			<a href="https://www.alekshnayder.com/" target="_blank" rel="noopener">
-				Alek Shnayder
-			</a>
+			<div>
+				Created with 🍷 by{' '}
+				<a href="https://www.alekshnayder.com/" target="_blank" rel="noopener">
+					Alek Shnayder
+				</a>
+			</div>
 			<Spacer>|</Spacer>
-			<IconLink
-				style={{ marginRight: '1rem' }}
-				href="mailto:swiftsheetapp@example.com?Subject=Hello"
-				target="_top"
-				rel="noopener"
-			>
-				<img src="./assets/images/mail.svg" alt="logo" height="22" />
-			</IconLink>
-			<IconLink
-				href="https://twitter.com/SheetSwift"
-				target="_blank"
-				rel="noopener"
-			>
-				<img src="./assets/images/twitter.svg" alt="logo" height="25" />
-			</IconLink>
+			<div>
+				<IconLink
+					style={{ marginRight: '1rem' }}
+					href="mailto:swiftsheetapp@example.com?Subject=Hello"
+					target="_top"
+					rel="noopener"
+				>
+					<img src="./assets/images/mail.svg" alt="logo" height="22" />
+				</IconLink>
+				<IconLink
+					href="https://twitter.com/SwiftSheetApp"
+					target="_blank"
+					rel="noopener"
+				>
+					<img src="./assets/images/twitter.svg" alt="logo" height="25" />
+				</IconLink>
+			</div>
 			<Spacer>|</Spacer>
-			<Link to="/privacy">Privacy Policy</Link>
+			<div>
+				<Link to="/privacy">Privacy Policy</Link>
+			</div>
 			<Spacer>|</Spacer>
-			<Link to="/terms">Terms of Service</Link>
+			<div>
+				<Link to="/terms">Terms of Service</Link>
+			</div>
 		</Styledfooter>
 	);
 }
@@ -43,21 +51,41 @@ export default function Footer() {
 const Styledfooter = styled(defaultStyle)`
 	box-sizing: border-box;
 	padding: 1rem;
-	margin-bottom: 0.5rem;
+	margin-top: 0.5rem;
 	text-align: center;
 	width: 100%;
+	display: flex;
+	flex-direction: row;
+	flex-wrap: wrap;
+	justify-content: center;
 
 	& a {
 		color: ${props => props.theme.color.text};
+	}
+
+	@media (max-width: 500px) {
+		flex-direction: column;
+		div {
+			margin-top: 0.5rem;
+			margin-bottom: 0.5rem;
+		}
 	}
 `;
 
 const Spacer = styled.span`
 	margin-left: 1rem;
 	margin-right: 1rem;
+
+	@media (max-width: 500px) {
+		display: none;
+	}
 `;
 
 const IconLink = styled.a`
 	position: relative;
-	top: 7px;
+	top: -5px;
+
+	@media (max-width: 500px) {
+		top: 0;
+	}
 `;
