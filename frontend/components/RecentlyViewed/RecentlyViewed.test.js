359b777284f3244e369572f1a1f1ad005f3aaa7c
Alek Shnayder <alek.shnayder@gmail.com>
2019-10-12 11:07:33 -0500

    Minor cleanup

--- frontend/components/RecentlyViewed/RecentlyViewed.test.js
+++ frontend/components/RecentlyViewed/RecentlyViewed.test.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import 'jest-styled-components';
 import renderer from 'react-test-renderer';
+import { render } from '@testing-library/react';
+import { LocalStorageMock } from '@react-mock/localstorage';
+import '@testing-library/jest-dom/extend-expect';
+import { ThemeProvider } from 'styled-components';
 
 import RecentlyViewed, { StyledCard } from './RecentlyViewed';
 import theme from '../../theme';
@@ -14,5 +18,18 @@ describe('RecentlyViewed', () => {
 	test('StyledCard matches the snapshot', () => {
 		const tree = renderer.create(<StyledCard theme={theme} />).toJSON();
 		expect(tree).toMatchSnapshot();
+	});
+
+	test('it should show nothing viewed recently', () => {
+		const mockHistory = [];
+		const ui = render(
+			<ThemeProvider theme={theme}>
+				<LocalStorageMock history={mockHistory}>
+					<RecentlyViewed />
+				</LocalStorageMock>
+			</ThemeProvider>
+		);
+
+		expect(ui.getByText('No recently viewed items')).toBeInTheDocument();
 	});
 });
